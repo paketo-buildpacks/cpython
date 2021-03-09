5e2ccf911f4e1b76b325499e69c13ecc2f68925f
Arjun Sreedharan <asreedharan@vmware.com>
2021-03-09 20:31:10 +0000

    Docs: Add go docs

--- detect.go
+++ detect.go
@@ -7,18 +7,33 @@ import (
 )
 
 //go:generate faux --interface VersionParser --output fakes/version_parser.go
+
+// VersionParser defines the interface for determining the version of Cpython.
 type VersionParser interface {
 	ParseVersion(path string) (version string, err error)
 }
 
+// BuildPlanMetadata is the buildpack specific data included in build plan
+// requirements.
 type BuildPlanMetadata struct {
-	Version       string `toml:"version"`
+	// Version denotes the version constraint to be requested in the requirements.
+	Version string `toml:"version"`
+
+	// VersionSource denotes the source of the version information. This may be
+	// used by the consumer of the metadata to determine the priority of this
+	// version request.
 	VersionSource string `toml:"version-source"`
 }
 
+// Detect will return a packit.DetectFunc that will be invoked during the
+// detect phase of the buildpack lifecycle.
+//
+// Detect will contribute a Build Plan that provides cpython.
 func Detect(buildpackYMLParser VersionParser) packit.DetectFunc {
 	return func(context packit.DetectContext) (packit.DetectResult, error) {
 		var requirements []packit.BuildPlanRequirement
+
+		// TODO(restructure): Remove legacy requirements
 		var requirementsLegacy []packit.BuildPlanRequirement
 
 		version, err := buildpackYMLParser.ParseVersion(filepath.Join(context.WorkingDir, "buildpack.yml"))
