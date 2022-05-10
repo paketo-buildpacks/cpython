e13999aae781e3273d1f8dc5415262c484aa0c28
Rob Dimsdale-Zucker <robert.dimsdale@gmail.com>
2022-05-10 09:11:34 -0400

    Update integration tests to support pack v0.26.0

--- integration/default_test.go
+++ integration/default_test.go
@@ -146,7 +146,7 @@ func testDefault(t *testing.T, context spec.G, it spec.S) {
 
 				// check that legacy SBOM is included via metadata
 				container2, err = docker.Container.Run.
-					WithCommand("cat /layers/config/metadata.toml").
+					WithCommand("cat /layers/sbom/launch/sbom.legacy.json").
 					Execute(image.ID)
 				Expect(err).NotTo(HaveOccurred())
 
@@ -154,11 +154,7 @@ func testDefault(t *testing.T, context spec.G, it spec.S) {
 					cLogs, err := docker.Container.Logs.Execute(container2.ID)
 					Expect(err).NotTo(HaveOccurred())
 					return cLogs.String()
-				}).Should(And(
-					ContainSubstring("[[bom]]"),
-					ContainSubstring(`name = "CPython`),
-					ContainSubstring("[bom.metadata]"),
-				))
+				}).Should(ContainSubstring(`"name":"CPython"`))
 
 				// check that all required SBOM files are present
 				Expect(filepath.Join(sbomDir, "sbom", "launch", strings.ReplaceAll(buildpackInfo.Buildpack.ID, "/", "_"), "cpython", "sbom.cdx.json")).To(BeARegularFile())
