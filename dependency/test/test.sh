4d0e365f366f5bd941e4c4587100556ac7c07cce
Jerico Pena <jericop@users.noreply.github.com>
2025-08-22 17:11:06 -0400

    Add support for multi-arch linux amd64 and arm64 dependencies (#986)

--- dependency/test/test.sh
+++ dependency/test/test.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
-shopt -s inherit_errexit
+
+if [[ "$(uname)" != "Darwin" ]]; then
+  shopt -s inherit_errexit
+fi
 
 parent_dir="$(cd "$(dirname "$0")" && pwd)"
 
@@ -9,6 +12,8 @@ main() {
   local tarball_path expectedVersion
   tarball_path=""
   expectedVersion=""
+  expectedOS=""
+  expectedArch=""
 
   while [ "${#}" != 0 ]; do
     case "${1}" in
@@ -19,6 +24,16 @@ main() {
 
       --expectedVersion)
         expectedVersion="${2}"
+        shift 2
+        ;;
+
+      --expectedOS)
+        expectedOS="${2}"
+        shift 2
+        ;;
+
+      --expectedArch)
+        expectedArch="${2}"
         shift 2
         ;;
 
@@ -44,48 +59,58 @@ main() {
 
   echo "Outside image: tarball_path=${tarball_path}"
   echo "Outside image: expectedVersion=${expectedVersion}"
+  echo "Outside image: expectedOS=${expectedOS}"
+  echo "Outside image: expectedArch=${expectedArch}"
 
-  if [[ $(basename "${tarball_path}") == *"bionic"* ]]; then
-    echo "Running bionic test..."
+  # Expects tarball_path filename such as: python_3.10.18_linux_amd64_bionic_ed1b62b3.tgz
+  tarballVersion=$(echo $(basename "${tarball_path}") | cut -d_ -f2) # ie: 3.10.18
+  os=$(echo $(basename "${tarball_path}") | cut -d_ -f3 | tr '_' '/') # ie: linux
+  arch=$(echo $(basename "${tarball_path}") | cut -d_ -f4 | tr '_' '/') # ie: amd64
+  target=$(echo $(basename "${tarball_path}") | cut -d_ -f5) # ie: bionic
+
+  if [[ "${tarballVersion}" != "${expectedVersion}" ]]; then
+    echo "Tarball version (${tarballVersion}) does not match expected version (${expectedVersion})"
+    exit 1
+  fi
+
+  if [[ "${expectedOS}" != "" && "${expectedOS}" != "${os}" ]]; then
+    echo "Tarball os (${os}) does not match expectedOS (${expectedOS})"
+    exit 1
+  fi
+
+  if [[ "${expectedArch}" != "" && "${expectedArch}" != "${arch}" ]]; then
+    echo "Tarball arch (${arch}) does not match expectedArch (${expectedArch})"
+    exit 1
+  fi
+
+  # When --expectedOS and --expectedArch are provided, the --platform arg is passed to docker build and run commands.
+  # This assumes the runner has qemu and buildkit set up, and that the docker daemon and cli experimental features are enabled.
+  docker_platform_arg=""
+  if [[ "${expectedOS}" != "" && "${expectedArch}" != "" ]]; then
+    docker_platform_arg="--platform ${os}/${arch}"
+    echo "Outside image: docker commands will be called with ${docker_platform_arg}"
+  fi
+
+  if [[ -f "$target.Dockerfile" ]]; then
+    echo "Building image with dockerfile ${target}.Dockerfile..."
     docker build \
+      --quiet \
       --tag test \
-      --file bionic.Dockerfile \
+      --file "${target}.Dockerfile" \
+      ${docker_platform_arg} \
       .
 
+    echo "Running ${target} test..."
     docker run \
       --rm \
       --volume "$(dirname -- "${tarball_path}"):/tarball_path" \
-      test \
-      --tarballPath "/tarball_path/$(basename "${tarball_path}")" \
-      --expectedVersion "${expectedVersion}"
-  elif [[ $(basename -- "${tarball_path}") == *"jammy"* ]]; then
-    echo "Running jammy test..."
-    docker build \
-      --tag test \
-      --file jammy.Dockerfile \
-      .
-
-    docker run \
-      --rm \
-      --volume "$(dirname -- "${tarball_path}"):/tarball_path" \
-      test \
-      --tarballPath "/tarball_path/$(basename "${tarball_path}")" \
-      --expectedVersion "${expectedVersion}"
-  elif [[ $(basename -- "${tarball_path}") == *"noble"* ]]; then
-    echo "Running noble test..."
-    docker build \
-      --tag test \
-      --file noble.Dockerfile \
-      .
-
-    docker run \
-      --rm \
-      --volume "$(dirname -- "${tarball_path}"):/tarball_path" \
+      ${docker_platform_arg} \
       test \
       --tarballPath "/tarball_path/$(basename "${tarball_path}")" \
       --expectedVersion "${expectedVersion}"
   else
-    echo "bionic or jammy not found - skipping tests"
+    echo "No dockerfile found for ${target}, expected ${target}.Dockerfile to exist"
+    exit 1
   fi
 }
 
